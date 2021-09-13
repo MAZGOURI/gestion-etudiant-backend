1a3dfd5dd5fb26a0557e4202e31e67f1ffacd857
youssef_mazgouri <52420305+MAZGOURI@users.noreply.github.com>
2021-09-13 15:36:54 +0100

    Update Snippet.java

--- src/main/resources/snippet/Snippet.java
+++ src/main/resources/snippet/Snippet.java
@@ -3,7 +3,7 @@ package snippet;
 public class Snippet {
 	spring.datasource.url=jdbc:postgresql://localhost:5432/GestionEtudiant
 	spring.datasource.username=GestionEtudiant
-	spring.datasource.password=pokimon2
+	spring.datasource.password=root
 	spring.jpa.hibernate.ddl-auto=update
 	
 }
